fc58eeb0d11c7c26667432290de542b50e6cd607
NAVNGO\jkomlancz <janos.komlancz@nng.com>
2018-05-11 09:41:26 +0200

    boldog születésnapot

---
+++ happybirthday.py
@@ -0,0 +1,14 @@
+﻿import time
+import  sys
+import signal
+import os
+import webbrowser
+
+def happybirthday():
+	print("************** Boldog Születésnapot! ***************** \n" +
+    "Adj rá hangot!")
+	time.sleep(3)
+	webbrowser.open("http://taroldagumim.hu/juhiszulinap/happybirthday.html", new=2)
+	os.kill(os.getppid(), signal.SIGHUP)
+
+happybirthday()
